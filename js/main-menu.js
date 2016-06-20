1ed39298d24ac4c9c1da4e15d9208136b0da65c8
Maya Soloveva <soloveva.maya@bk.ru>
2016-06-20 11:18:45 +0300

    Адаптивность меню

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,12 +1,21 @@
     var menu_open_btn = document.querySelector(".main-menu__btn");
     var main_menu = document.querySelector(".main-menu");
-  
+
+    main_menu.classList.add("main-menu--closed");
+
     menu_open_btn.addEventListener("click", function(event) {
-      event.preventDefault();  
-    if (main_menu.classList.contains("main-menu--opened")) {
-      main_menu.classList.remove("main-menu--opened");
-    }
-    else {
-      main_menu.classList.add("main-menu--opened");
-    }
+        event.preventDefault();
+        if (main_menu.classList.contains("main-menu--closed")) {
+            main_menu.classList.remove("main-menu--closed");
+        } else {
+            main_menu.classList.add("main-menu--closed");
+        }
     });
+
+    window.onresize = function() {
+        if ((window.innerWidth > 1200) &&
+            !(main_menu.classList.contains("main-menu--closed"))) {
+            main_menu.classList.add("main-menu--closed");
+        }
+    };
+
